912485907d06ccdd316d82e69904d66981d28b7b
kartikgavani-max <kartikgavani@gmail.com>
2026-01-02 21:11:39 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,36 @@
+public class Pattern{
+    public static void hallow_rectangle(int row,int col) {
+       
+        for(int i=1;i<=row;i++)
+        {
+            for(int j=1;j<=col;j++)
+            {
+                if(i==1 || i==row || j==1 || j==col)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void inverted_haf_pyramid(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        hallow_rectangle(5, 5);
+        inverted_haf_pyramid(5);
+    }
+}
