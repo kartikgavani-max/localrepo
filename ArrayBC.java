bdc03941367f8efa0965901d58d0456d57bb659b
kartikgavani-max <kartikgavani@gmail.com>
2026-03-07 23:37:57 +0530

    Add files via upload

---
+++ ArrayBC.java
@@ -0,0 +1,24 @@
+public class ArrayBC {
+    public static int BinarySearch(int num[],int key){
+        int start=0,end=num.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(num[mid]==key){
+                return mid;
+            }
+            if(num[mid]<=key){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10,12,14};
+        int key=0;
+        System.out.println("Key Found at :"+ BinarySearch(num, key));
+    }
+     
+}
