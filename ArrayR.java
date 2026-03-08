e4c69614098538eb144e3682ad93ed15c90a77c0
kartikgavani-max <kartikgavani@gmail.com>
2026-03-08 23:05:15 +0530

    Add files via upload

---
+++ ArrayR.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class ArrayR{
+       
+    public static void reverse(int num[]){
+        int start=0,end=num.length-1;
+
+        while (start<=end) {
+            int temp = num[end];
+            num[end] = num[start];
+            num[start] = temp;
+
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10};
+
+        reverse(num);
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+        System.out.println();
+    }
+}
